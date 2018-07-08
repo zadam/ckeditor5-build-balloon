be90574a433ed08518b36c8fffe437c431e96f34
azivner <adam.zivner@gmail.com>
2018-07-08 23:22:41 +0200

    added sourceMappingURL transformation to correct URL

--- build-and-update.sh
+++ build-and-update.sh
@@ -2,4 +2,7 @@
 
 npm run build
 
+# pointing the source map file to correct location
+sed -i -e 's/sourceMappingURL=ckeditor.js.map/sourceMappingURL=libraries\/ckeditor\/ckeditor.js.map/g' build/ckeditor.js
+
 cp build/ckeditor.* ~/trilium/src/public/libraries/ckeditor/
