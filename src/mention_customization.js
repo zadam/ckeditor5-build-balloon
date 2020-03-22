b9cc4507a190e9b07dcbed229dcbc9a05c92d0d5
zadam <zadam.apps@gmail.com>
2020-03-22 18:26:29 +0100

    reference link is also inserted by mentions

--- src/mention_customization.js
+++ src/mention_customization.js
@@ -1,15 +1,24 @@
-export default function MentionCustomization(editor) {
-	// Downcast the model 'mention' text attribute to a view <a> element.
-	editor.conversion.for('downcast').attributeToElement({
-		model: 'mention',
-		view: (modelAttributeValue, viewWriter) => {
-			// Do not convert empty attributes (lack of value means no mention).
-			if (!modelAttributeValue) {
-				return;
-			}
+import Command from "@ckeditor/ckeditor5-core/src/command";
 
-			return viewWriter.createAttributeElement('a', { 'href': '#' + modelAttributeValue.path });
-		},
-		converterPriority: 'high'
-	});
+export default function MentionCustomization(editor) {
+	setTimeout(() => {
+		editor.commands.add('mention', new CustomMentionCommand(editor));
+	}, 1000);
+}
+
+class CustomMentionCommand extends Command {
+	execute(options) {
+		const model = this.editor.model;
+		const document = model.document;
+		const selection = document.selection;
+
+		const range = options.range || selection.getFirstRange();
+
+		model.change(writer => {
+			// override the selection or at least the beginning @ character
+			model.insertContent(writer.createText(''), range);
+
+			this.editor.execute('referenceLink', {notePath: options.mention.path});
+		});
+	}
 }
